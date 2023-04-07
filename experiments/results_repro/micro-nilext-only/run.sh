741a4c6c228efad7ed30211ff24c88729a8bb975
Shriram <shriramathreya786@gmail.com>
2023-04-06 20:14:01 -0500

    number of nodes and ip change

--- experiments/results_repro/micro-nilext-only/run.sh
+++ experiments/results_repro/micro-nilext-only/run.sh
@@ -23,7 +23,7 @@ for n in 2 5 10 15 20 40; do
 for i in 1; do
 for code in orig; do
         # set the batch parameter to 1 for no batching (for paxos-nobatch variant)
-        ../../remote-throughput.py --medium $r_medium --code $code --time $r_time --run $i --cluster $r_cluster --sync no --user $r_user --workload $workload --num_nodes 3 --target_system_name $r_system --sync_rep_factor 3 --num_clients $n --leader_reads yes --batch 1
+        ../../remote-throughput.py --medium $r_medium --code $code --time $r_time --run $i --cluster $r_cluster --sync no --user $r_user --workload $workload --num_nodes 5 --target_system_name $r_system --sync_rep_factor 5 --num_clients $n --leader_reads yes --batch 1
         sleep 2
 done
 done
@@ -38,11 +38,11 @@ for n in 2 5 12 25 35 40 50 70 100; do
 for i in 1; do
 for code in orig; do
         # set the batch parameter to 20 batched Paxos...this is the value for which paxos performed the best
-        ../../remote-throughput.py --medium $r_medium --code $code --time $r_time --run $i --cluster $r_cluster --sync no --user $r_user --workload $workload --num_nodes 3 --target_system_name $r_system --sync_rep_factor 3 --num_clients $n --leader_reads yes --batch 20
+        ../../remote-throughput.py --medium $r_medium --code $code --time $r_time --run $i --cluster $r_cluster --sync no --user $r_user --workload $workload --num_nodes 5 --target_system_name $r_system --sync_rep_factor 5 --num_clients $n --leader_reads yes --batch 20
         sleep 2
 done
 done
-done
+done                            
 
 rm -rf ./$workload.$r_system.orig.*
 mv ../../$workload.$r_system.orig.* .
@@ -53,7 +53,7 @@ for n in 2 5 12 15 25 35 50 70 100; do
 for i in 1; do
 for code in rtop; do
         # set the batch parameter to 20 batched Paxos...this is the value for which paxos performed the best
-        ../../remote-throughput.py --medium $r_medium --code $code --time $r_time --run $i --cluster $r_cluster --sync no --user $r_user --workload $workload --num_nodes 3 --target_system_name $r_system --sync_rep_factor 3 --num_clients $n --leader_reads yes --batch 64
+        ../../remote-throughput.py --medium $r_medium --code $code --time $r_time --run $i --cluster $r_cluster --sync no --user $r_user --workload $workload --num_nodes 5 --target_system_name $r_system --sync_rep_factor 3 --num_clients $n --leader_reads yes --batch 64
         sleep 2
 done
 done
