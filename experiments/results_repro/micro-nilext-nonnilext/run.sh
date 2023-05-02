704c7dbb9d52ab41169edf881886debb88eca321
Shriram <shriramathreya786@gmail.com>
2023-05-01 20:35:40 -0500

    nonnilext run.sh update

--- experiments/results_repro/micro-nilext-nonnilext/run.sh
+++ experiments/results_repro/micro-nilext-nonnilext/run.sh
@@ -11,7 +11,7 @@ workload='e' #nilext and non-nilext writes
 # i: iteration -- paper reported average of 3 runs. For reasonable experimental times for AE, we will just do one run
 # code: orig - original paxos, rtop - skyros
 
-n=10 # 10 clients for this experiment
+# n=10 # 10 clients for this experiment
 
 # initial setup
 #chmod 0400 ../../pems/$r_cluster.pem
@@ -24,10 +24,12 @@ n=10 # 10 clients for this experiment
 
 # paxos
 code=orig
-for nonnilpercent in 1 2 5 10 20 40 100; do
+for n in 2 5 10 20; do
+for nonnilpercent in 100; do
 for i in 1; do
         ../../remote-throughput.py --medium $r_medium --code $code --time $r_time --run $i --cluster $r_cluster --sync no --user $r_user --workload $workload --num_nodes 5 --target_system_name $r_system --sync_rep_factor 3 --num_clients $n --leader_reads yes --batch 20 --workload_trace_prefix e$nonnilpercent
         sleep 2
+done
 done
 done
 
@@ -37,10 +39,12 @@ mv ../../ee*orig* .
 
 # skyros
 code=rtop
-for nonnilpercent in 1 2 5 10 20 40 100; do
+for n in 2 5 10 20; do
+for nonnilpercent in 100; do
 for i in 1; do
         ../../remote-throughput.py --medium $r_medium --code $code --time $r_time --run $i --cluster $r_cluster --sync no --user $r_user --workload $workload --num_nodes 5 --target_system_name $r_system --sync_rep_factor 3 --num_clients $n --leader_reads yes --batch 64 --workload_trace_prefix e$nonnilpercent
         sleep 2
+done
 done
 done
 
